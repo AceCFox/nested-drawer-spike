0abe723ae8ca5d094ddd5c5bab227660d35229e8
Ace Fox <afox09@gmail.com>
2020-08-30 15:52:47 -0500

    edit buttons

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Accordion from '@material-ui/core/Accordion';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
+import {AccordionDetails, Button} from '@material-ui/core';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import Divider from '@material-ui/core/Divider';
 import AccordionActions from '@material-ui/core/AccordionActions';
+import EditIcon from '@material-ui/icons/Edit';
+
 
 
 const styles = theme => ({
@@ -16,9 +17,8 @@ const styles = theme => ({
     width: '100%',
    // display: 'flex',
     alignItems: 'center',
-    padding: 8,
-    justifyContent: 'flex-end',
-    direction: 'column'
+    justify: 'center',
+    textAlign: 'center',
   },
   heading: {
     fontSize: theme.typography.pxToRem(15),
@@ -62,7 +62,7 @@ class App extends React.Component {
         <br/>
         <Accordion>
           <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography className={classes.heading}>Host Site 1</Typography>
+            <Typography className={classes.heading}>First Host Site</Typography>
             <Typography className={classes.secondaryHeading}>Host Site Address</Typography>
           </AccordionSummary>
           <AccordionDetails>
@@ -82,7 +82,9 @@ class App extends React.Component {
                          eros, vitae egestas augue. Duis vel est augue.
                       </AccordionDetails>
                       <AccordionActions>
-
+                        <Button variant='contained'>
+                          <EditIcon/>Edit Device
+                        </Button>
                       </AccordionActions>
                     </Accordion>
                   </AccordionDetails>
@@ -93,21 +95,25 @@ class App extends React.Component {
                         <Typography className={classes.secondaryHeading}>Device Info</Typography>
                       </AccordionSummary>
                       <AccordionDetails>
-                        The rest of the information about this device. Lorem Ipsem Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas
-                        eros, vitae egestas augue. Duis vel est augue.
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                       Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                       </AccordionDetails>
+                      <AccordionActions>
+                        <Button variant='contained'>
+                          <EditIcon/>Edit Device
+                        </Button>
+                      </AccordionActions>
                     </Accordion>
                 </AccordionDetails>
+                <AccordionActions>
+                  <Button variant='contained'>
+                    <EditIcon/>Edit Breaker
+                  </Button>
+                </AccordionActions>
               </Accordion>  
           </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography className={classes.heading}>Host site 2</Typography>
-            <Typography className={classes.secondaryHeading}>
-              You are currently not an owner
-            </Typography>
-          </AccordionSummary>
           <AccordionDetails>
           <Accordion>
               <AccordionSummary expandIcon={<ExpandMoreIcon />}>
@@ -124,8 +130,76 @@ class App extends React.Component {
                         The rest of the information about this device. Lorem Ipsem Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas
                          eros, vitae egestas augue. Duis vel est augue.
                       </AccordionDetails>
+                      <AccordionActions>
+                        <Button variant= 'contained'>
+                          <EditIcon/> Edit Device
+                        </Button>
+                      </AccordionActions>
                     </Accordion>
-                    <Divider/>
+                  </AccordionDetails>
+                  <AccordionDetails>
+                    <Accordion>
+                      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                        <Typography className={classes.heading}>Device 2</Typography>
+                        <Typography className={classes.secondaryHeading}>Device Info</Typography>
+                      </AccordionSummary>
+                      <AccordionDetails>
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                       Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                      </AccordionDetails>
+                      <AccordionActions>
+                        <Button variant='contained'>
+                          <EditIcon/>Edit Device
+                        </Button>
+                      </AccordionActions>
+                    </Accordion>
+                </AccordionDetails>
+                <AccordionActions>
+                        <Button variant='contained'>
+                          <EditIcon/>Edit Breaker
+                        </Button>
+                      </AccordionActions>
+              </Accordion>  
+          </AccordionDetails>
+          <AccordionActions>
+            <Button variant='contained'>
+              <EditIcon/>Edit Host Site
+            </Button>
+          </AccordionActions>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography className={classes.heading}>Second Host Site</Typography>
+            <Typography className={classes.secondaryHeading}>
+              You are currently not an owner
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+          <Accordion>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography className={classes.heading}>First Breaker Name</Typography>
+                <Typography className={classes.secondaryHeading}>Breaker Limit</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Accordion>
+                      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                        <Typography className={classes.heading}>Device 1</Typography>
+                        <Typography className={classes.secondaryHeading}>Device Info</Typography>
+                      </AccordionSummary>
+                      <AccordionDetails>
+                        The rest of the information about this device. Lorem Ipsem Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas
+                         eros, vitae egestas augue. Duis vel est augue.
+                      </AccordionDetails>
+                      <AccordionActions>
+                        <Button variant='contained'>
+                          <EditIcon/>Edit Device
+                        </Button>
+                      </AccordionActions>
+                    </Accordion>
+                    </AccordionDetails>
+                    <AccordionDetails>
                     <Accordion>
                       <AccordionSummary expandIcon={<ExpandMoreIcon />}>
                         <Typography className={classes.heading}>Device 2</Typography>
@@ -135,17 +209,29 @@ class App extends React.Component {
                         The rest of the information about this device. Lorem Ipsem Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas
                         eros, vitae egestas augue. Duis vel est augue.
                       </AccordionDetails>
+                      <AccordionActions>
+                        <Button variant='contained'>
+                          <EditIcon/>Edit Device
+                        </Button>
+                      </AccordionActions>
                     </Accordion>
                 </AccordionDetails>
+                <AccordionActions>
+                <Button variant= 'contained'>
+                  <EditIcon/> Edit Breaker
+                </Button>
+                </AccordionActions>
               </Accordion>  
           </AccordionDetails>
+          <AccordionActions>
+            <Button variant= 'contained'>
+              <EditIcon/> Edit Host Site
+            </Button>
+          </AccordionActions>
         </Accordion>
         <Accordion>
           <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography className={classes.heading}>Advanced settings</Typography>
-            <Typography className={classes.secondaryHeading}>
-              Filtering has been entirely disabled for whole web server
-            </Typography>
+            <Typography className={classes.heading}>Third Host Site</Typography>
           </AccordionSummary>
           <AccordionDetails>
             <Typography>
@@ -153,10 +239,15 @@ class App extends React.Component {
               eros, vitae egestas augue. Duis vel est augue.
             </Typography>
           </AccordionDetails>
+          <AccordionActions>
+            <Button variant='contained'>
+              <EditIcon/>Edit Host Site
+            </Button>
+          </AccordionActions>
         </Accordion>
         <Accordion>
           <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography className={classes.heading}>Personal data</Typography>
+            <Typography className={classes.heading}>Fourth Host Site</Typography>
           </AccordionSummary>
           <AccordionDetails>
             <Typography>
@@ -164,7 +255,19 @@ class App extends React.Component {
               eros, vitae egestas augue. Duis vel est augue.
             </Typography>
           </AccordionDetails>
+          <AccordionActions>
+            <Button variant='contained'>
+              <EditIcon/>Edit Host Site
+            </Button>
+          </AccordionActions>
         </Accordion>
+        <br/>
+        <Button
+          size ="large"
+          variant='contained'
+          color ='primary'>
+          Submit Onboarding
+        </Button>
       </div>
     );
   }
